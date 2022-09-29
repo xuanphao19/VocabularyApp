25b5a62eb6bf17c2699642e38776bec649f7448c
Nguyễn Thanh Hòa <83102917+xuanphao19@users.noreply.github.com>
2022-09-29 14:33:31 +0700

    Thay anh js

--- index.js
+++ index.js
@@ -1,79 +1,81 @@
-var card = document.querySelector('.flip-card-inner');
-var cardNext = document.querySelector('#next');
-var suggestions = document.querySelector('#suggestions');
-var formMessage = document.querySelector('.App-message');
-var suggestionsMsg = document.querySelector('.suggestions');
-var answerElement = document.querySelector('#answer');
-var questionFrontId = document.querySelector('#questionFront');
-var submit = document.querySelector('#submit');
-var lengthArr = document.querySelector('.lengthArr');
-var submitResult = document.querySelector('#submitResult');
+var appElement = document.querySelector('#App-1');
+var card = appElement.querySelector('.flip-card-inner');
+var cardNext = appElement.querySelector('#next');
+var suggestions = appElement.querySelector('#suggestions');
+var formMessage = appElement.querySelector('.App-message');
+var suggestionsMsg = appElement.querySelector('.suggestions');
+var answerElement = appElement.querySelector('#answer');
+var questionFrontId = appElement.querySelector('#questionFront');
+var submit = appElement.querySelector('#submit');
+var lengthArr = appElement.querySelector('.lengthArr');
+var submitResult = appElement.querySelector('#submitResult');
+var flipCardFrontEle = appElement.querySelector('.flip-card-front');
+var suggestionsElement = appElement.querySelector('#suggestionsBack');
 var lengths;
 var i = 0;
 
 var vocabularyEnglish = [
-  ['Leisure', 'thời gian rảnh rỗi'],
-  ['Leisure activity', 'hoạt động lúc rảnh rỗi'],
-  ['Hanging out', 'đi chơi với bạn bè'],
-  ['Hang out', 'tụ tập, đi chơi'],
-  ["Let's hang out this weekend", 'Cuối tuần này đi chơi đi!'],
-  ['Window shopping', 'V: Đi ngắm đồ bày trong các cửa hàng'],
-  ['Go window shopping', 'N: Ngắm đồ bày trong các cửa hàng'],
-  ['Pet training', 'N: Huấn luyện thú cưng'],
-  ['Train a pet', 'V: Huấn luyện thú cưng'],
-  ['Making crafts', 'làm đồ thủ công'],
-  ['Craft kit', 'bộ dụng cụ làm đồ thủ công'],
-  ['Bracelet', 'vòng đeo tay'],
-  ['Check out something', 'kiểm tra vật gì đó có phù hợp hay không'],
-  ["Right up someone's street", 'đúng vị/ sở thích của ai'],
-  ['Playing beach games', 'hoạt động chơi các môn thể thao trên bãi biển'],
-  ['Play beach games', 'chơi các môn thể thao trên bãi biển'],
-  ['DIY: Do-It-Yourself', 'tự làm, tự sửa (đồ gì)'],
-  ['Satisfied', 'hài lòng'],
-  ['Nguyễn Thanh Hòa', 'Nhập: tên của bố bạn'],
-  ["What is your mother's name?", 'Tên mẹ của bạn là gì?'],
-  ["What is your sister's name?", 'Tên chị gái của bạn là gì'],
-  ["What is your brother's name?", 'tên em trai của bạn là gì'],
-  ['DIY project', 'N:dự án, kế hoạch tự làm (đồ gì)'],
-  ['Mountain biking', 'n: hoạt động đạp xe leo núi'],
-  ['Go mountain biking', 'n: đạp xe leo núi'],
-  ['Socialising', 'n: hoạt động giao lưu'],
-  ['Socialise ', 'v: giao lưu'],
-  ['Texting', 'n: nhắn tin'],
-  ['Text', 'v: nhắn tin'],
-  ['Surfing the Internet', 'n: lướt web'],
-  ['Surf the Internet', 'v: lướt web'],
-  ['hanging', 'They liked...out together when they were kids'],
-  ['eating', 'I fancy ... out with friends at the weekend'],
-  ['Doing', '.... DIY brings you a lot of amazing benefits'],
-  ['Paddy', 'n: cánh đồng lúa'],
-  ['Rice', 'n: lúa, gạo, cơm'],
-  ['Rice straw', 'rơm'],
-  ['Harvest time', 'n: mùa thu hoạch, mùa gặt'],
-  ['Harvest - Collect', 'v: thu hoạch, gặt'],
-  ['Ripe', 'ad: chín'],
-  ['Buffalo-drawn cart', 'xe trâu kéo'],
-  ['Grill fish in rice straw', 'v: nướng cá bằng rơm'],
-  ['Fly a kite', 'v: thả diều'],
-  ['Herd the buffalo', 'chăn trâu'],
-  ['Flying kites in the wide open countryside is great fun', 'Viết lại vd(123) Fly a kite'],
-  ['Go herding the buffalo', 'v: chăn trâu'],
-  ['Herd the cattle', 'v: chăn bò'],
-  ['Herd the sheep', 'v: chăn cừu'],
-  ['Livestock', 'gia súc'],
-  ['Ride a buffalo', 'cưỡi trâu'],
-  ['Pick fruit', 'hái trái cây'],
-  ['Pick wild flowers', 'hái hoa dại'],
-  ['Collect hay', 'lượm cỏ khô'],
-  ['Collect water', 'lấy nước'],
-  ['Convenient', 'ad: thuận tiện'],
-  ['Inconvenient', 'ad: bất tiện'],
-  ['Peaceful', 'ad: yên bình'],
-  ['Hospitable', 'hiếu khách'],
-  ['Vietnamese people are very hospitable', 'Viết lại vd(123) với từ: Hospitable'],
-  ['Generous', 'hào phóng'],
-  ['Optimistic', 'lạc quan'],
-
+  ['Leisure', 'thời gian rảnh rỗi', "<img src='./Hatrang.png' alt='Avatar' />;", 'Gợi ý'],
+  ['Leisure activity', 'hoạt động lúc rảnh rỗi', "<img src='./Screenshot_20210503-120123_Gallery.jpg' alt='Avatar' />", 'Gợi ý'],
+  // ['Hanging out', 'đi chơi với bạn bè'],
+  // ['Hang out', 'tụ tập, đi chơi'],
+  // ["Let's hang out this weekend", 'Cuối tuần này đi chơi đi!'],
+  // ['Window shopping', 'V: Đi ngắm đồ bày trong các cửa hàng'],
+  // ['Go window shopping', 'N: Ngắm đồ bày trong các cửa hàng'],
+  // ['Pet training', 'N: Huấn luyện thú cưng'],
+  // ['Train a pet', 'V: Huấn luyện thú cưng'],
+  // ['Making crafts', 'làm đồ thủ công'],
+  // ['Craft kit', 'bộ dụng cụ làm đồ thủ công'],
+  // ['Bracelet', 'vòng đeo tay'],
+  // ['Check out something', 'kiểm tra vật gì đó có phù hợp hay không'],
+  // ["Right up someone's street", 'đúng vị/ sở thích của ai'],
+  // ['Playing beach games', 'hoạt động chơi các môn thể thao trên bãi biển'],
+  // ['Play beach games', 'chơi các môn thể thao trên bãi biển'],
+  // ['DIY: Do-It-Yourself', 'tự làm, tự sửa (đồ gì)'],
+  // ['Satisfied', 'hài lòng'],
+  // ['Nguyễn Thanh Hòa', 'Nhập: tên của bố bạn'],
+  // ["What is your mother's name?", 'Tên mẹ của bạn là gì?'],
+  // ["What is your sister's name?", 'Tên chị gái của bạn là gì'],
+  // ["What is your brother's name?", 'tên em trai của bạn là gì'],
+  // ['DIY project', 'N:dự án, kế hoạch tự làm (đồ gì)'],
+  // ['Mountain biking', 'n: hoạt động đạp xe leo núi'],
+  // ['Go mountain biking', 'n: đạp xe leo núi'],
+  // ['Socialising', 'n: hoạt động giao lưu'],
+  // ['Socialise ', 'v: giao lưu'],
+  // ['Texting', 'n: nhắn tin'],
+  // ['Text', 'v: nhắn tin'],
+  // ['Surfing the Internet', 'n: lướt web'],
+  // ['Surf the Internet', 'v: lướt web'],
+  // ['hanging', 'They liked...out together when they were kids'],
+  // ['eating', 'I fancy ... out with friends at the weekend'],
+  // ['Doing', '.... DIY brings you a lot of amazing benefits'],
+  // ['Paddy', 'n: cánh đồng lúa'],
+  // ['Rice', 'n: lúa, gạo, cơm'],
+  // ['Rice straw', 'rơm'],
+  // ['Harvest time', 'n: mùa thu hoạch, mùa gặt'],
+  // ['Harvest - Collect', 'v: thu hoạch, gặt'],
+  // ['Ripe', 'ad: chín'],
+  // ['Buffalo-drawn cart', 'xe trâu kéo'],
+  // ['Grill fish in rice straw', 'v: nướng cá bằng rơm'],
+  // ['Fly a kite', 'v: thả diều'],
+  // ['Herd the buffalo', 'chăn trâu'],
+  // ['Flying kites in the wide open countryside is great fun', 'Viết lại vd(123) Fly a kite'],
+  // ['Go herding the buffalo', 'v: chăn trâu'],
+  // ['Herd the cattle', 'v: chăn bò'],
+  // ['Herd the sheep', 'v: chăn cừu'],
+  // ['Livestock', 'gia súc'],
+  // ['Ride a buffalo', 'cưỡi trâu'],
+  // ['Pick fruit', 'hái trái cây'],
+  // ['Pick wild flowers', 'hái hoa dại'],
+  // ['Collect hay', 'lượm cỏ khô'],
+  // ['Collect water', 'lấy nước'],
+  // ['Convenient', 'ad: thuận tiện'],
+  // ['Inconvenient', 'ad: bất tiện'],
+  // ['Peaceful', 'ad: yên bình'],
+  // ['Hospitable', 'hiếu khách'],
+  // ['Vietnamese people are very hospitable', 'Viết lại vd(123) với từ: Hospitable'],
+  // ['Generous', 'hào phóng'],
+  // ['Optimistic', 'lạc quan'],
   // ['', ''],
   // ['', ''],
   // ['', ''],
@@ -152,13 +154,17 @@ var vocabularyEnglish = [
   // ['', ''],
   // ['', ''],
   // ['', ''],
-  ['Paddy field', 'Nhập nội dung bạn vừa nghe:<audio controls><source src="https://www.tienganh123.com/file/phothong/lop8-moi/unit2/lesson1/vocab/audio/1.mp3"></audio>'],
-  ['Harvest time', 'Nhập nội dung bạn vừa nghe:<audio controls><source src="https://www.tienganh123.com/file/phothong/lop8-moi/unit2/lesson1/vocab/audio/4.mp3"></audio>'],
-  [
-    'Go herding the buffalo(es)',
-    'Nhập nội dung bạn vừa nghe:<audio controls><source src="https://www.tienganh123.com/file/phothong/lop8-moi/unit2/lesson1/vocab/audio/11.mp3"></audio>',
-  ],
-  ['Herd the buffalo(es)', 'Nhập nội dung bạn vừa nghe:<audio controls><source src="https://www.tienganh123.com/file/phothong/lop8-moi/unit2/lesson1/vocab/audio/10.mp3"></audio>'],
+  // [
+  //   'Livestock include farm animals such as buffalo, cow, goat or sheep',
+  //   'Viết lại nội dung vừa nghe:<audio controls><source src="https://www.tienganh123.com/file/phothong/lop8-moi/unit2/lesson2/vocab/audio/3-1.mp3"></audio>',
+  // ],
+  // ['Paddy field', 'Viết lại nội dung vừa nghe:<audio controls><source src="https://www.tienganh123.com/file/phothong/lop8-moi/unit2/lesson1/vocab/audio/1.mp3"></audio>'],
+  // ['Harvest time', 'Viết lại nội dung vừa nghe:<audio controls><source src="https://www.tienganh123.com/file/phothong/lop8-moi/unit2/lesson1/vocab/audio/4.mp3"></audio>'],
+  // [
+  //   'Go herding the buffaloes',
+  //   'Viết lại nội dung vừa nghe:<audio controls><source src="https://www.tienganh123.com/file/phothong/lop8-moi/unit2/lesson1/vocab/audio/11.mp3"></audio>',
+  // ],
+  // ['Herd the buffaloes', 'Viết lại nội dung vừa nghe:<audio controls><source src="https://www.tienganh123.com/file/phothong/lop8-moi/unit2/lesson1/vocab/audio/10.mp3"></audio>'],
 ];
 
 https: var songs = [
@@ -207,7 +213,6 @@ cardNext.addEventListener('click', function () {
       return formMessage;
     }
   };
-
   submitResult.innerHTML = '';
   if (answerElement.value === randomTerm[0]) {
     i += 1;
@@ -216,8 +221,12 @@ cardNext.addEventListener('click', function () {
 
   suggestionsMsg.innerHTML = '';
   if (handleTest(answerElement) === undefined) {
-    card.classList.toggle('is-flipped');
+    card.classList.remove('is-flipped');
+    if (randomTerm[2]) {
+      flipCardFrontEle.innerHTML = `${randomTerm[2]}`;
+    }
   }
+
   return i;
 });
 
@@ -225,6 +234,10 @@ suggestions.addEventListener('click', function () {
   submitResult.innerHTML = '';
   formMessage.innerHTML = '';
   suggestionsMsg.innerHTML = '';
+  if (randomTerm[3] === 'Gợi ý') {
+    suggestionsElement.innerHTML = `${randomTerm[0]}`;
+  }
+  card.classList.toggle('is-flipped');
   if (answerElement.value === '') {
     suggestionsMsg.innerHTML = `Lười học là "Bệnh cần chống như chống giặc!"`;
     return;
@@ -239,11 +252,11 @@ submit.addEventListener('click', function () {
   suggestionsMsg.innerHTML = '';
   if (!answerElement.value) {
     suggestionsMsg.innerHTML = '';
-    submitResult.innerHTML = `Cần trả lời đúng ít nhất 10 câu!`;
+    submitResult.innerHTML = `Bạn cần phải trả lời trước khi bấm nộp bài`;
     return;
   }
   if (i < 10) {
-    submitResult.innerHTML = `Bạn chưa hoàn thành đủ 10 câu trả lời đúng!`;
+    submitResult.innerHTML = `Hoàn thành đủ 10 câu trả lời đúng <br> mới được nộp bài!`;
   }
   if (i >= 10) {
     submitResult.classList.add('correctResult');
